b12772c38f681380dff6c0c458bab3fdb36c38a8
Josh Burke <joshb2699@gmail.com>
2016-03-29 17:52:07 +0100

    Updated

---
+++ server/Server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var path = require('path');
+var port = 8080
+
+    app = express();
+    
+app.use(express.static(__dirname+'/public'));
+app.set('view engine', 'ejs')
+app.set('views', './server/views')
+
+var indexView = function(req,res) {
+    res.render('index.ejs');
+    }
+    
+app.get('/image', function(req, res) {
+     res.render('image', {image_url: req.query.image});
+});
+
+app.use('/public', 
+    express.static(path.join(__dirname, '../public'))
+);
+
+app.get('*', indexView);
+
+app.listen(port);
+
+console.log('Server running on port ' + port)
